beaeed2900b794792242f9d157d38b3782409dc3
devops-developer <83109540+devops-developer@users.noreply.github.com>
2021-05-20 21:11:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,9 +9,7 @@ public class HelloWorld {
     System.out.println("Happy sankranti");
     System.out.println("Happy sankranti");
      System.out.println("Happy Ugadi.....");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy Ugadi.....");
-  
+   
 
   }
 }
